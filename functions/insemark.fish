498bd353ee8ab7b50d4a343d98218a9349c3e7af
David Fernandez <i.am.david.fernandez@gmail.com>
2017-06-01 16:40:47 +1000

    <DF> Encapsulated bookmark management (set/get) in functions. Added colour to presented bookmark list to aid selection.

--- functions/insemark.fish
+++ functions/insemark.fish
@@ -1,3 +1,27 @@
+function __add_mark -a bookmark_file
+    set -e argv[1]
+    echo "$PWD# $argv "(date) >> $bookmark_file
+end
+
+function __get_marks_for_selection -a bookmark_file
+    for mark in (cat $bookmark_file)
+        set -l elements (string split "#" $mark)
+        set_color normal
+        echo -n $elements[1]
+        set_color blue
+        echo " #"$elements[2]
+    end
+end
+
+function __get_mark -a bookmark_file
+    __get_marks_for_selection $bookmark_file | eval $inse_selector | read bookmark
+    if test -n "$bookmark"
+        set -l bookmark_dir (echo $bookmark | sed -e 's/\s*#.*//g')
+        echo $bookmark_dir
+    end
+end
+
+
 function insemark -d "Create/navigate to bookmarked directories with interactive fuzzy selection."
 
     set -l bookmark_file "$HOME/.inse.bookmarks"
@@ -7,16 +31,17 @@ function insemark -d "Create/navigate to bookmarked directories with interactive
 
         switch $arg
             case '--set'
-                echo "$PWD #$argv "(date) >> $bookmark_file
+                __add_mark $bookmark_file $argv
                 return
         end
     end
 
     if test -e $bookmark_file
-        cat $bookmark_file | eval $inse_selector | read bookmark
-        if test -n "$bookmark"
-           set -l bookmark_dir (echo $bookmark | sed -e 's/\s*#.*//g')
+        __get_mark $bookmark_file | read bookmark_dir
+        if test -n "$bookmark_dir"
            cd $bookmark_dir
         end
+    else
+        echo "No bookmarks stored."
     end
 end
