8624d45c8a5580960b68575ab7baf1afe03c694b
David Fernandez <i.am.david.fernandez@gmail.com>
2017-06-01 13:58:02 +1000

    <DF> Streamlined file set creation.

--- functions/insegit.fish
+++ functions/insegit.fish
@@ -1,11 +1,7 @@
 function insegit -d "Git command wrapper with interactive fuzzy file selection."
     set -l tmp (tempfile)
-    git status --porcelain | eval $inse_selector  > $tmp
-    set -l file_set
-    for f in (cat $tmp | awk '{print $2}')
-        set file_set $file_set $f
-    end
+    git status --porcelain | eval $inse_selector > $tmp
+    set -l file_set (cat $tmp | awk '{print $2}' | string join " ")
     rm -f $tmp
-
     commandline --replace "git $argv $file_set"
 end
--- functions/insesvn.fish
+++ functions/insesvn.fish
@@ -1,11 +1,7 @@
 function insesvn -d "Subversion command wrapper with interactive fuzzy file selection."
     set -l tmp (tempfile)
     svn status | eval $inse_selector > $tmp
-    set -l file_set
-    for f in (cat $tmp | awk '{print $2}')
-        set file_set $file_set $f
-    end
+    set -l file_set (cat $tmp | awk '{print $2}' | string join " ")
     rm -f $tmp
-
     commandline --replace "svn $argv $file_set"
 end
